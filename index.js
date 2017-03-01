25eab7139062702702d9598666270e535953a37e
Sri Nannapaneni <srikanna@amazon.com>
2017-02-28 22:43:22 -0500

    time change + directory structure+1

--- index.js
+++
@@ -1,9 +0,0 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/New_York");
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in New_York is: ' + currentTime.toString(),
-    });
-};
