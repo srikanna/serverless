f6edb3188ca86f24f0fd6871bce1673f245a6aa5
Sri Nannapaneni <srikanna@amazon.com>
2017-03-02 22:22:43 -0500

    testing port of spacefinder API

---
+++ spacefinderfn/environment.js
@@ -0,0 +1,13 @@
+'use strict';
+// Auto-generated file, do not modify directly
+
+var config = {
+  "REGION": "us-east-1",
+  "PROFILE_IMAGES_S3_BUCKET": "spacefinder-api-development-stack-userdatabucket-1narb4ygft8z4",
+  "API_ENDPOINT": "https://p56srs6og3.execute-api.us-east-1.amazonaws.com/development",
+  "USER_POOL_ID": "us-east-1_n109nZnTb",
+  "CLIENT_ID": "3an2a0ooumalompa2e85vhdlhs",
+  "IDENTITY_POOL_ID": "us-east-1:ef262bfe-93b0-40f4-905c-3de390245fea"
+};
+
+module.exports = { config };
